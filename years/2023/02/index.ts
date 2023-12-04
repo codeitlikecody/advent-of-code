31c7c190b849675b706c2f5fea43b7a8c8cb75b7
Chris Cody <3807221+aussiechris@users.noreply.github.com>
2023-12-04 12:33:18 +1100

    2023 d2 p2

--- years/2023/02/index.ts
+++ years/2023/02/index.ts
@@ -26,15 +26,10 @@ async function p2023day2_part1(input: string, ...params: any[]) {
 		const gameNumber = gameTitle[0].match(/Game (\d+)/)?.[1];
 
 		let possibleGame = true;
-		// console.log(`Game Number: ${gameNumber}`);
 		for (const game of games) {
-			// console.log(`Selection: ${game}`);
 			const green = Number(game.match(/(\d+) green/)?.[1]);
 			const blue = Number(game.match(/(\d+) blue/)?.[1]);
 			const red = Number(game.match(/(\d+) red/)?.[1]);
-			// console.log(`Green: ${green}`);
-			// console.log(`Blue: ${blue}`);
-			// console.log(`Red: ${red}`);
 			if (red > RED_MAX || green > GREEN_MAX || blue > BLUE_MAX) {
 				possibleGame = false;
 				break;
@@ -48,7 +43,38 @@ async function p2023day2_part1(input: string, ...params: any[]) {
 }
 
 async function p2023day2_part2(input: string, ...params: any[]) {
-	return "Not implemented";
+	const lines = input.split("\n");
+	let sum = 0;
+
+	const RED_MAX = 12;
+	const GREEN_MAX = 13;
+	const BLUE_MAX = 14;
+
+	for (const line of lines) {
+		const gameTitle = line.split(":");
+		const games = gameTitle[1].split(";");
+		const gameNumber = gameTitle[0].match(/Game (\d+)/)?.[1];
+		var minRed = 0;
+		var minGreen = 0;
+		var minBlue = 0;
+
+		for (const game of games) {
+			const green = Number(game.match(/(\d+) green/)?.[1]);
+			const blue = Number(game.match(/(\d+) blue/)?.[1]);
+			const red = Number(game.match(/(\d+) red/)?.[1]);
+			if (minRed <= red) {
+				minRed = red;
+			}
+			if (minGreen <= green) {
+				minGreen = green;
+			}
+			if (minBlue <= blue) {
+				minBlue = blue;
+			}
+		}
+		sum += minRed * minGreen * minBlue;
+	}
+	return sum;
 }
 
 async function run() {
@@ -62,7 +88,16 @@ async function run() {
 			expected: `8`,
 		},
 	];
-	const part2tests: TestCase[] = [];
+	const part2tests: TestCase[] = [
+		{
+			input: `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+			Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+			Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+			Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+			Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`,
+			expected: `2286`,
+		},
+	];
 
 	// Run tests
 	test.beginTests();
